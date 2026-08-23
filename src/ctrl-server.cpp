255fa25a93efc3af49f10e985f38f8a0796782b4
Alexander Schulz <code@schulzalex.de>
2026-08-23 16:21:03 +0200

    disable startup counter

--- src/ctrl-server.cpp
+++ src/ctrl-server.cpp
@@ -139,7 +139,7 @@ void CtrlServer::Tick10ms()
 
 void CtrlServer::Tick100ms()
 {
-	static uint8_t startupCounter = 0;
+	//static uint8_t startupCounter = 0;
 
 	helper->DEBUG_TIMER(DEBUGLEVEL_TRACE, "100ms");
 
@@ -158,83 +158,83 @@ void CtrlServer::Tick100ms()
 	// DeviceTypeAndProperty every 2 seconds, Headamp-Message every 2 seconds (Names every 10 seconds)
 	mixer->fpga->AES50Tick();
 
-	if (startupCounter < 100)
-	{
-		startupCounter++;
+	// if (startupCounter < 100)
+	// {
+	// 	startupCounter++;
 
-		// 	// if (startupCounter == 10)
-		// 	// {
-		// 	// 	// the gate, the dynamics and the EQ-settings are not loaded correctly on first load, so load it again after a short time
-		// 	// 	config->LoadConfig(0);
+	// 	// 	// if (startupCounter == 10)
+	// 	// 	// {
+	// 	// 	// 	// the gate, the dynamics and the EQ-settings are not loaded correctly on first load, so load it again after a short time
+	// 	// 	// 	config->LoadConfig(0);
 
-		// 	// 	// in the following lines the default configuration is set so that the users of the beta-version
-		// 	// 	// can start with a working system
+	// 	// 	// 	// in the following lines the default configuration is set so that the users of the beta-version
+	// 	// 	// 	// can start with a working system
 
-		// 	// 	// route channel 1-4 to effects using post-fader tapping
-		// 	// 	for (uint8_t i = 0; i < 8; i++)
-		// 	// 	{
-		// 	// 		config->Set(ROUTING_DSP_OUTPUT, DSP_BUF_IDX_DSPCHANNEL + (i / 2), 40 + i);
-		// 	// 		config->Set(ROUTING_DSP_OUTPUT_TAPPOINT, int(DSP_TAP::POST_FADER), 40 + i);
-		// 	// 	}
+	// 	// 	// 	// route channel 1-4 to effects using post-fader tapping
+	// 	// 	// 	for (uint8_t i = 0; i < 8; i++)
+	// 	// 	// 	{
+	// 	// 	// 		config->Set(ROUTING_DSP_OUTPUT, DSP_BUF_IDX_DSPCHANNEL + (i / 2), 40 + i);
+	// 	// 	// 		config->Set(ROUTING_DSP_OUTPUT_TAPPOINT, int(DSP_TAP::POST_FADER), 40 + i);
+	// 	// 	// 	}
 
-		// 	// 	// set AUX7/8 to MONITOR L/R
-		// 	// 	config->Set(ROUTING_DSP_OUTPUT, DSP_BUF_IDX_MONLEFT, 38);
-		// 	// 	config->Set(ROUTING_DSP_OUTPUT, DSP_BUF_IDX_MONRIGHT, 39);
-		// 	// 	config->Set(ROUTING_DSP_OUTPUT_TAPPOINT, int(DSP_TAP::POST_FADER), 38);
-		// 	// 	config->Set(ROUTING_DSP_OUTPUT_TAPPOINT, int(DSP_TAP::POST_FADER), 39);
+	// 	// 	// 	// set AUX7/8 to MONITOR L/R
+	// 	// 	// 	config->Set(ROUTING_DSP_OUTPUT, DSP_BUF_IDX_MONLEFT, 38);
+	// 	// 	// 	config->Set(ROUTING_DSP_OUTPUT, DSP_BUF_IDX_MONRIGHT, 39);
+	// 	// 	// 	config->Set(ROUTING_DSP_OUTPUT_TAPPOINT, int(DSP_TAP::POST_FADER), 38);
+	// 	// 	// 	config->Set(ROUTING_DSP_OUTPUT_TAPPOINT, int(DSP_TAP::POST_FADER), 39);
 
-		// 	// 	// set volume of FX-return to 0dBfs
-		// 	// 	// set volume of FX-return to -120dBfs
-		// 	// 	for (int i = 0; i < 8; i++)
-		// 	// 	{
-		// 	// 		config->Set(CHANNEL_VOLUME, VOLUME_MIN, 40 + i);
-		// 	// 	}
+	// 	// 	// 	// set volume of FX-return to 0dBfs
+	// 	// 	// 	// set volume of FX-return to -120dBfs
+	// 	// 	// 	for (int i = 0; i < 8; i++)
+	// 	// 	// 	{
+	// 	// 	// 		config->Set(CHANNEL_VOLUME, VOLUME_MIN, 40 + i);
+	// 	// 	// 	}
 
-		// 	// 	// set default FXes in FX slots
-		// 	// 	// mixer->dsp->DSP2_SetFx(0, FX_TYPE::REVERB, 2); // on first load this effect has a bug, so we have to disable it a bit later
-		//     //     // mixer->dsp->DSP2_SetFx(1, FX_TYPE::CHORUS, 2);
-		//     //     // mixer->dsp->DSP2_SetFx(2, FX_TYPE::DELAY, 2);
-		// 	// 	mixer->dsp->DSP2_SetFx(0, FX_TYPE::NONE, 2);
-		// 	// 	mixer->dsp->DSP2_SetFx(1, FX_TYPE::NONE, 2);
-		// 	// 	mixer->dsp->DSP2_SetFx(2, FX_TYPE::NONE, 2);
-		//     //     mixer->dsp->DSP2_SetFx(3, FX_TYPE::NONE, 2);
-		//     //     mixer->dsp->DSP2_SetFx(4, FX_TYPE::NONE, 2);
-		//     //     mixer->dsp->DSP2_SetFx(5, FX_TYPE::NONE, 2);
-		//     //     mixer->dsp->DSP2_SetFx(6, FX_TYPE::NONE, 2);
-		//     //     mixer->dsp->DSP2_SetFx(7, FX_TYPE::NONE, 2);
+	// 	// 	// 	// set default FXes in FX slots
+	// 	// 	// 	// mixer->dsp->DSP2_SetFx(0, FX_TYPE::REVERB, 2); // on first load this effect has a bug, so we have to disable it a bit later
+	// 	//     //     // mixer->dsp->DSP2_SetFx(1, FX_TYPE::CHORUS, 2);
+	// 	//     //     // mixer->dsp->DSP2_SetFx(2, FX_TYPE::DELAY, 2);
+	// 	// 	// 	mixer->dsp->DSP2_SetFx(0, FX_TYPE::NONE, 2);
+	// 	// 	// 	mixer->dsp->DSP2_SetFx(1, FX_TYPE::NONE, 2);
+	// 	// 	// 	mixer->dsp->DSP2_SetFx(2, FX_TYPE::NONE, 2);
+	// 	//     //     mixer->dsp->DSP2_SetFx(3, FX_TYPE::NONE, 2);
+	// 	//     //     mixer->dsp->DSP2_SetFx(4, FX_TYPE::NONE, 2);
+	// 	//     //     mixer->dsp->DSP2_SetFx(5, FX_TYPE::NONE, 2);
+	// 	//     //     mixer->dsp->DSP2_SetFx(6, FX_TYPE::NONE, 2);
+	// 	//     //     mixer->dsp->DSP2_SetFx(7, FX_TYPE::NONE, 2);
 
-		// 	// 	// set FX-settings to wet on slot 1-4
-		// 	// 	config->Set(FX_REVERB_DRY, 0, 0); // fx-slot 1
-		// 	// 	config->Set(FX_REVERB_WET, 1, 0); // fx-slot 1
-		// 	// 	config->Set(FX_CHORUS_MIX, 1, 1); // fx-slot 2		
-		// 	// }
+	// 	// 	// 	// set FX-settings to wet on slot 1-4
+	// 	// 	// 	config->Set(FX_REVERB_DRY, 0, 0); // fx-slot 1
+	// 	// 	// 	config->Set(FX_REVERB_WET, 1, 0); // fx-slot 1
+	// 	// 	// 	config->Set(FX_CHORUS_MIX, 1, 1); // fx-slot 2		
+	// 	// 	// }
 
-		// 	// if (startupCounter == 40) {
-		// 	// 	// disable effect as on first start of the effect some parts in
-		// 	// 	// the external memory gets corrupted. This needs more debugging
-		// 	// 	// for now stop-restart is fine
-		// 	// 	mixer->dsp->DSP2_SetFx(0, FX_TYPE::NONE, 2);
-		// 	// 	mixer->dsp->DSP2_SetFx(2, FX_TYPE::NONE, 2);
-		// 	// }
+	// 	// 	// if (startupCounter == 40) {
+	// 	// 	// 	// disable effect as on first start of the effect some parts in
+	// 	// 	// 	// the external memory gets corrupted. This needs more debugging
+	// 	// 	// 	// for now stop-restart is fine
+	// 	// 	// 	mixer->dsp->DSP2_SetFx(0, FX_TYPE::NONE, 2);
+	// 	// 	// 	mixer->dsp->DSP2_SetFx(2, FX_TYPE::NONE, 2);
+	// 	// 	// }
 
-		// 	// if (startupCounter == 50) {
-		// 	// 	// renable effect
-		// 	// 	// mixer->dsp->DSP2_SetFx(0, FX_TYPE::REVERB, 2);
-		// 	// 	// mixer->dsp->DSP2_SetFx(2, FX_TYPE::DELAY, 2);
-		// 	// }
+	// 	// 	// if (startupCounter == 50) {
+	// 	// 	// 	// renable effect
+	// 	// 	// 	// mixer->dsp->DSP2_SetFx(0, FX_TYPE::REVERB, 2);
+	// 	// 	// 	// mixer->dsp->DSP2_SetFx(2, FX_TYPE::DELAY, 2);
+	// 	// 	// }
 
-		// 	// if (startupCounter == 60) {
+	// 	// 	// if (startupCounter == 60) {
 				
-		// 	// }
+	// 	// 	// }
 
-		if (startupCounter == 30)
-		{
-			float value = 0;
-			mixer->dsp->spi->QueueDspData(0, 'a', 0, 0, 1, &value);
+	// 	// if (startupCounter == 30)
+	// 	// {
+	// 	// 	float value = 0;
+	// 	// 	mixer->dsp->spi->QueueDspData(0, 'a', 0, 0, 1, &value);
 
-			config->Set(CHANNEL_MUTE, 1, 35);
-		}
-	}
+	// 	// 	config->Set(CHANNEL_MUTE, 1, 35);
+	// 	// }
+	// }
 }
 
 void CtrlServer::Tick1000ms()
